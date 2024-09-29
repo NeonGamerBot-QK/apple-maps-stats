6e401d509486dde82f2e86f439b5bcb2dedc6ac2
Saahil <neon@saahild.com>
2024-09-28 23:34:56 -0400

    feat: tried to add parsers

--- index.ts
+++ index.ts
@@ -1,5 +1,17 @@
+//@ts-ignore
 import { MailListener } from "mail-listener5"
-
+enum Phrases {
+    STARTING, 
+    UPDATED,
+    ENDING,
+    UNK  = -1
+}
+type StrResult = {
+    phrase: string,
+    type: Phrases,
+    location: string,
+    timestamp?: string
+}
 if(!Bun.env["MAIL_USERNAME"]) {
     console.log("MAIL_USERNAME not set")
     process.exit(1)
@@ -12,6 +24,42 @@ if(!Bun.env["MAIL_HOST"]) {
     console.log("MAIL_HOST not set")
     process.exit(1)
     }
+    const startANDEnd = (content: string, start: string, end: string) => {
+        if(content.includes(start) && content.includes(end)) {
+            console.log(content)
+            return true
+            }
+            return false
+            }
+const parseStr = (content: string) => {
+    const arrivalRegex = /I will arrive at (.*) around (.*)\. I'll let you know if i'm running late\./
+    const updatedRegex = /My updated arrival time to (.*) is now around (.*)\./
+    const endingRegex = /I'm arriving at (.*) soon/
+    const result: StrResult = {
+        phrase: content,
+        type: Phrases.UNK,
+        location: "",
+        }
+        if(content.match(arrivalRegex)) {
+            result.type = Phrases.STARTING
+            result.location = arrivalRegex.exec(content)![1]
+            result.timestamp = arrivalRegex.exec(content)![2]
+        }
+        else if(content.match(updatedRegex)) {
+                result.type = Phrases.UPDATED
+                result.location = updatedRegex.exec(content)![1] 
+                result.timestamp = updatedRegex.exec(content)![2] 
+                }
+        else if(content.match(endingRegex)) {
+                    result.type = Phrases.ENDING
+                    result.location = endingRegex.exec(content)![1]
+                    }
+                    return result
+
+}
+// console.log(parseStr("I'm arriving at [location] soon."))
+// console.log(parseStr("My updated arrival time to [location] is now around [timestamp]."))
+// console.log(parseStr("I will arrive at <location> around <timestamp>. I'll let you know if i'm running late."))
 const mailListener = new MailListener({
     username:Bun.env["MAIL_USERNAME"].toString(),
     password:Bun.env["MAIL_PASSWORD"].toString(),
@@ -28,7 +76,7 @@ const mailListener = new MailListener({
     fetchUnreadOnStart: true,
     // mailParserOptions: {streamAttachments: true},
     attachments: true,
-    // attachmentOptions: { directory: "attachments/" }
+    attachmentOptions: { directory: "attachments/" }
   });
 
     //@ts-ignore  shush
@@ -37,10 +85,25 @@ const mailListener = new MailListener({
             console.log("no attachments")
             return
             }
-        console.log(mail.attachments[0].content.toString(), "mail")
-      });
+            const content = mail.attachments[0].content.toString()
+        // console.log(mail.attachments[0].content.toString(), "mail")
+        // based on content create json map
+        const parsed = parseStr(content)
+    console.debug(parsed)
+    switch(parsed.type) {
+        case Phrases.STARTING:
+            console.log("starting")
+            break
+            case Phrases.UPDATED:
+                console.log("updated")
+                break
+                case Phrases.ENDING:
+                    console.log("ending")
+                    break
+    }
+    });
   mailListener.start();
-      mailListener.on("error", function(error){
+      mailListener.on("error", function(error:any){
         console.log(error)
         });
         mailListener.on("end", function(){
