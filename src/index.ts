4e28ed97e8190954039b0b7b723b56f3d10ce967
Saahil <neon@saahild.com>
2024-09-29 20:53:44 -0400

    feat: db usage (im gonna switch to json db tmrw)

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,14 @@ if (!Bun.env["MAIL_HOST"]) {
   console.log("MAIL_HOST not set");
   process.exit(1);
 }
-const db = new Database("db.db", {
+const db = new Database("main.db", {
   create: true,
   readwrite: true,
 });
 // enable WAL
 db.exec("PRAGMA journal_mode = WAL;");
 db.run(
-  "CREATE TABLE IF NOT EXISTS apple_maps_stats (email TEXT PRIMARY KEY, last_updated TEXT, location TEXT, timestamp TEXT, heartbeat INT DEFAULT 0)",
+  `CREATE TABLE IF NOT EXISTS apple_maps_stats (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, last_updated TEXT, location TEXT, timestamp TEXT, heartbeat INT DEFAULT 0, arrived_at INT DEFAULT 0)`,
 );
 
 //test db
@@ -61,16 +61,34 @@ mailListener.on("mail", function (mail, seqno, attributes) {
   // based on content create json map
   const parsed = parseStr(content);
   console.debug(parsed);
-  console.debug(`from`, mail.from[0]);
+  console.debug(`from`, mail.from);
   switch (parsed.type) {
     case Phrases.STARTING:
       console.log("starting");
+      if(!parsed.timestamp) return; 
+      db.query(`INSERT INTO apple_maps_stats (email, last_updated, location, timestamp) VALUES ($email, $last_updated, $loc, $t)`).run({
+        $email: mail.from.value[0].address,
+        $last_updated: Date.now(),
+        $loc: parsed.location!,
+        $t: parsed.timestamp!,
+        });
       break;
     case Phrases.UPDATED:
-      console.log("updated");
+      if(!parsed.timestamp) return;
+      db.query(`UPDATE apple_maps_stats SET last_updated = $last_updated, location = $loc, timestamp = $t WHERE email = $email`).run({
+        $email: mail.from.value[0].address,
+        $last_updated: Date.now(),
+        $loc: parsed.location!,
+        $t: parsed.timestamp!,
+        });
       break;
     case Phrases.ENDING:
       console.log("ending");
+      if(!parsed.timestamp) return;
+      db.query(`UPDATE apple_maps_stats SET arrived_at = $arrived_at WHERE email = $email`).run({
+        $email: mail.from.value[0].address,
+        $arrived_at: Date.now(),
+        });
       break;
   }
 });
